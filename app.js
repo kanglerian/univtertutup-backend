a2a820fda4b1e845328609ce2069baec0d0aa05c
kanglerian <kanglerian@gmail.com>
2022-11-26 22:11:29 +0700

    add cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let express = require('express');
 let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
+let cors = require('cors');
 
 let indexRouter = require('./routes/index');
 let levelRouter = require('./routes/level');
@@ -10,6 +11,7 @@ let registrarRouter = require('./routes/registrar');
 
 let app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
